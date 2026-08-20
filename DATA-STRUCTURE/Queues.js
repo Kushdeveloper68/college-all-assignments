57edc26760ed1d85f2023aa7e91510a79133e376
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-08-20 18:05:29 +0530

    queues code

---
+++ DATA-STRUCTURE/Queues.js
@@ -0,0 +1,29 @@
+// QUEUES -> ENQUEUE DEQUEUE
+
+let queue = []
+let front = -1
+let rear = -1
+let lenghtOfQueue = 30
+function ENQUEUE(element) {
+    if (rear >= lenghtOfQueue) return "OVERFLOW";
+     if (front == -1) front = 0;
+     rear += 1
+     queue[rear] = element
+    return "inserted";
+}
+
+function DEQUEUE() {
+    if (front == -1 && front > rear) return "UNDERFLOW";
+    queue[front] = null;
+    if (front == rear) front = rear = -1;
+    else front += 1;
+    return 'deleted'
+}
+
+console.log(ENQUEUE(10))
+console.log(ENQUEUE(20))
+console.log(ENQUEUE(30))
+console.log(ENQUEUE(40))
+console.log(DEQUEUE())
+console.log(DEQUEUE())
+console.log(queue)
