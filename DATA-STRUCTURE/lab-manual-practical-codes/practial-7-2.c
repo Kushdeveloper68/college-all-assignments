4b55074b849268cbd8aa9904517ba1ce42c79627
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-08-28 11:31:48 +0530

    practical 2nd of 7th

---
+++ DATA-STRUCTURE/lab-manual-practical-codes/practial-7-2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+// this function is from 6th practical.
+void creation_insertion_display_Node(struct Node **head, int item)
+{ // creation
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = item;
+    newNode->next = NULL;
+    // insertion
+    if (*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        struct Node *temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        // display
+    }
+
+    struct Node *temp = *head;
+
+    while (temp != NULL)
+    {
+        printf("%d ->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+};
+
+// this is for 7th practicaal
+void deletionfromtheEnd(struct Node **head)
+{
+    if (*head == NULL)
+    {
+        printf("list is empty");
+        return;
+    }
+    if ((*head)->next == NULL)
+    {
+        free(*head);
+        *head = NULL;
+    }
+    else
+    {
+        struct Node *temp = *head;
+        struct Node *prev;
+        while (temp->next != NULL)
+        {
+            printf("%d ->", temp->data);
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL;
+        free(temp);
+    }
+
+    printf("deleted\n");
+}
+
+int main()
+{
+    struct Node *head = NULL;
+    creation_insertion_display_Node(&head, 10);
+    creation_insertion_display_Node(&head, 20);
+    creation_insertion_display_Node(&head, 30);
+    deletionfromtheEnd(&head);
+    deletionfromtheEnd(&head);
+    deletionfromtheEnd(&head);
+    return 0;
+}
